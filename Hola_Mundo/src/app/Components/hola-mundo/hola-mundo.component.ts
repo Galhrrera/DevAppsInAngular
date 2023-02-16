13688bb701e6cda2a288b1e1dd8d20b1973da35f
Juan Esteban Galeano Herrera <jegaleanoherrera@gmail.com>
2023-02-16 17:56:45 -0500

    Hola mundo en angular. Se creó un componente para el hola mundo y se realizó string interpolation para llamar el valor de una varibale en html

---
+++ Hola_Mundo/src/app/Components/hola-mundo/hola-mundo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hola-mundo',
+  templateUrl: './hola-mundo.component.html',
+  styleUrls: ['./hola-mundo.component.scss']
+})
+export class HolaMundoComponent {
+  holaMundo:string = "Hola Mundo";
+}
